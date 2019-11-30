42d7f78efd1f96ce7e3bdefce4d672db3ec1c9b6
이성현 <dljdgus1211@naver.com>
2019-12-01 00:29:09 +0900

    add funtion

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace Imgae_Viewer
 {
@@ -20,9 +21,53 @@ namespace Imgae_Viewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string _ImagePath = null;
+
+        private string OpenFileDialog()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+
+            return dlg.FileName;
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void fileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            _ImagePath = OpenFileDialog();
+            //ViewBox.Source = new BitmapImage(new Uri(_ImagePath));
+        }
+
+        private void btnRightMenuHide_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbHideRightMenu", btnRightMenuHide, btnRightMenuShow, sideMenu);
+        }
+
+        private void btnRightMenuShow_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbShowRightMenu", btnRightMenuHide, btnRightMenuShow, sideMenu);
+        }
+
+        private void ShowHideMenu(string Storyboard, Button btnHide, Button btnShow, StackPanel pnl)
+        {
+            Storyboard sb = Resources[Storyboard] as Storyboard;
+            sb.Begin(pnl);
+
+            if (Storyboard.Contains("Show"))
+            {
+                btnHide.Visibility = System.Windows.Visibility.Visible;
+                btnShow.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else if (Storyboard.Contains("Hide"))
+            {
+                btnHide.Visibility = System.Windows.Visibility.Hidden;
+                btnShow.Visibility = System.Windows.Visibility.Visible;
+            }
         }
     }
 }
