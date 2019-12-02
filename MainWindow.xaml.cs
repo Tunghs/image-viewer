dc20a8fa3380964b6542486597638e080de874ff
이성현 <dljdgus1211@naver.com>
2019-12-02 19:41:14 +0900

    Add operation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,9 +13,26 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Animation;
+using System.IO;
 
 namespace Imgae_Viewer
 {
+
+    class MyData
+    {
+        public int dataNo { get; set; }
+        public string dataName { get; set; }
+
+        private static List<MyData> instance;
+
+        public static List<MyData> GetInstance()
+        {
+            if (instance == null)
+                instance = new List<MyData>();
+
+            return instance;
+        }
+    }
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
@@ -23,28 +40,54 @@ namespace Imgae_Viewer
     {
         private static string _ImagePath = null;
 
+        // 데이터 바인딩 방법.
+        private void data()
+        {
+            MyData.GetInstance().Add(new MyData() { dataNo = 1, dataName = "fileA" });
+
+            imageListView.ItemsSource = MyData.GetInstance();
+        }
+
         private string OpenFileDialog()
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            Nullable<bool> result = dlg.ShowDialog();
+            dlg.InitialDirectory = "c:\\";
 
-            return dlg.FileName;
+            if(dlg.ShowDialog() == true)
+            {
+                return dlg.FileName;
+            }
+            else
+            {
+                return null;
+            }
         }
 
-
+        
         public MainWindow()
         {
             InitializeComponent();
-            
-
         }
 
+        // 파일 열기
         private void fileOpen_Click(object sender, RoutedEventArgs e)
         {
             _ImagePath = OpenFileDialog();
             ViewBox.Source = new BitmapImage(new Uri(_ImagePath));
+            // 가져온 이미지 경로 내 모든 이미지들을 리스트에 추가해줌.
         }
 
+        private void file_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+        }
+
+        private void file_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+                ViewBox.Source = new BitmapImage(new Uri(file));
+        }
 
         // ---- 사이드 애니메이션 구현부 ---
         // 목록 메뉴
