631ee3a41fc042e7779f74140d8032cf91710a85
Tunghs <juk08843@gmail.com>
2024-05-10 10:39:54 +0900

    Feat: "ViewModelBase 추가"

---
+++ src/ImageViewer/Bases/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace ImageViewer.Bases
+{
+    public abstract class ViewModelBase : ObservableValidator
+    {
+        public ViewModelBase() { }
+
+        public virtual void Cleanup()
+        {
+            WeakReferenceMessenger.Default.Cleanup();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Cleanup();
+            }
+        }
+    }
+}
