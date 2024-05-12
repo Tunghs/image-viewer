8a8679144580df722a5f81be1a0cef24b4eacac0
Tunghs <juk08843@gmail.com>
2024-05-12 21:11:40 +0900

    Feat: "Add Popup window"

---
+++ src/ImageViewer/PopupViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using ImageViewer.Bases;
+
+namespace ImageViewer
+{
+    public partial class PopupViewModel : ViewModelBase
+    {
+        #region UI Properties
+        [ObservableProperty]
+        private ViewModelBase _popupVM;
+        #endregion
+    }
+}
---
+++ src/ImageViewer/PopupWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ImageViewer.Bases;
+
+using Wpf.Ui.Controls;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// PopupWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PopupWindow : FluentWindow, IDialog
+    {
+        public PopupWindow()
+        {
+            this.DataContext = new PopupViewModel();
+            InitializeComponent();
+        }
+    }
+}
