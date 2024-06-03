e523fda9164e67fec1520a38a69763027d6a681e
Tunghs <juk08843@gmail.com>
2024-06-03 11:16:37 +0900

    Feat: "FixedSizeStack 추가"
    * 잘못된 컨셉으로 추가한 클래스 제거

---
+++ src/ImageViewer/Data/FixedSizeStack.cs
@@ -0,0 +1,37 @@
+﻿namespace ImageViewer.Data
+{
+    public class FixedSizeStack<T>
+    {
+        private T[] _buffer;
+        private int top = 0;
+
+        public FixedSizeStack(int lenght)
+        {
+            _buffer = new T[lenght];
+        }
+        
+        public void Push(T item)
+        {
+            if (top < _buffer.Length)
+            {
+                _buffer[top] = item;
+                top = top + 1;
+            }
+            else
+            {
+                for (int index = 0; index < _buffer.Length - 1; index++)
+                {
+                    _buffer[index] = _buffer[index + 1];
+                }
+                _buffer[top] = item;
+            }
+        }
+
+        public T Pop()
+        {
+            T item = _buffer[top];
+            top = top - 1;
+            return item;
+        }
+    }
+}
