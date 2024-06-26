a52c0cb15d88b77ef9043fda643e7bf0e564ff35
Tunghs <juk08843@gmail.com>
2024-06-26 14:25:29 +0900

    Feat: "이미지 이동 관련 스낵바 추가"
    * 첫 번째 마지막 이미지에서 알람 보이도록 스낵바 추가

--- src/ImageViewer/MainWindowViewModel.cs
+++ src/ImageViewer/MainWindowViewModel.cs
@@ -3,35 +3,44 @@ using CommunityToolkit.Mvvm.DependencyInjection;
 using CommunityToolkit.Mvvm.Input;
 
 using ImageViewer.Bases;
-using ImageViewer.Data;
 using ImageViewer.PopupWindows;
 using ImageViewer.Services;
 
 using System.IO;
 using System.Windows.Input;
 
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+
 namespace ImageViewer
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
         #region Fields
-        private IFileControlService _controller;
-        private readonly IDialogService dialogService;
-        #endregion
 
-        public MainWindowViewModel(IDialogService dialogService)
+        private readonly IFileControlService _controller;
+        private readonly ISnackbarService _snackbarService;
+        private readonly IDialogService _dialogService;
+
+        #endregion Fields
+
+        public MainWindowViewModel()
         {
-            this.dialogService = dialogService;
+            _dialogService = Ioc.Default.GetService<IDialogService>();
+            _snackbarService = Ioc.Default.GetService<ISnackbarService>();
             _controller = Ioc.Default.GetService<IFileControlService>();
             _controller.Changed += OnControlChanged;
         }
 
         #region UI Properties
+
         [ObservableProperty]
         private string _title;
-        #endregion
+
+        #endregion UI Properties
 
         #region Command
+
         [RelayCommand]
         private void OnButtonClick(string @param)
         {
@@ -40,9 +49,11 @@ namespace ImageViewer
                 case "OpenSetting":
                     OpenSettingWindow();
                     break;
+
                 case "Crop":
                     // Crop();
                     break;
+
                 default:
                     break;
             }
@@ -51,25 +62,46 @@ namespace ImageViewer
         [RelayCommand]
         private void OnKeyDown(KeyEventArgs e)
         {
-            if (e.Key == Key.Left)
+            switch (e.Key)
             {
+                case Key.Left:
+                    _controller.Previous();
+                    break;
 
+                case Key.Right:
+                    _controller.Next();
+                    break;
+
+                default:
+                    break;
             }
         }
-        #endregion
+
+        #endregion Command
 
         #region Methods
+
         private void OpenSettingWindow()
         {
             // Popup1View 팝업 띄우기
-            dialogService.Show(new PopupViewModel(), "HI", 500, 650, typeof(PopupWindow));
+            _dialogService.Show(new PopupViewModel(), "HI", 500, 650, typeof(PopupWindow));
             //dialogService.Dialog.ShowDialog();
         }
 
         private void OnControlChanged(object? sender, FileChangedEventArgs e)
         {
-            Title = $"{Path.GetFileName(e.FileName)} ( {e.Index} / {e.TotalCount} )";
+            Title = $"{Path.GetFileName(e.FileName)} ( {e.Index + 1} / {e.TotalCount} )";
+
+            if (e.Index == 0)
+            {
+                _snackbarService.Show("Information", "This is the first image.", ControlAppearance.Secondary, new SymbolIcon(SymbolRegular.Info28), TimeSpan.FromSeconds(2));
+            }
+            else if (e.Index == e.TotalCount - 1)
+            {
+                _snackbarService.Show("Information", "This is the last image.", ControlAppearance.Secondary, new SymbolIcon(SymbolRegular.Info28), TimeSpan.FromSeconds(2));
+            }
         }
-        #endregion
+
+        #endregion Methods
     }
 }
