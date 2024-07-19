61a72d8afd79a0b9d467d10222cc4b3cd59508dc
Tunghs <juk08843@gmail.com>
2024-07-19 11:23:16 +0900

    Feat: "ImageProcessing 프로젝트 추가"

---
+++ src/ImageViewer.Core/ImageProcessing/ImageSimpleEditor.cs
@@ -0,0 +1,22 @@
+﻿using OpenCvSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageViewer.Core.ImageProcessing
+{
+    public class ImageSimpleEditor
+    {
+
+        public (int width, int height) GetImageSize(string filePath)
+        {
+            using (Mat img = Cv2.ImRead(filePath, ImreadModes.Unchanged))
+            {
+                return (img.Width, img.Height);
+            }
+        }
+    }
+}
