5e3a757d0c0ce342405f84953b5bf8ef05324e2a
Tunghs <juk08843@gmail.com>
2025-01-22 17:33:02 +0900

    Feat: "Add resize method"

--- src/ImageViewer.Core/ImageProcessing/ImageSimpleEditor.cs
+++ src/ImageViewer.Core/ImageProcessing/ImageSimpleEditor.cs
@@ -1,12 +1,5 @@
 ﻿using OpenCvSharp;
 
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ImageViewer.Core.ImageProcessing
 {
     public class ImageSimpleEditor
@@ -26,6 +19,15 @@ namespace ImageViewer.Core.ImageProcessing
             using (Mat dst = img.Resize(new OpenCvSharp.Size(width, height), 0, 0, InterpolationFlags.Cubic))
             {
                 Cv2.ImWrite(dstPath ,dst);
+            }
+        }
+
+        public void Resize(string srcPath, string dstPath, double ratio)
+        {
+            using (Mat img = Cv2.ImRead(srcPath, ImreadModes.Unchanged))
+            using (Mat dst = img.Resize(new OpenCvSharp.Size((int)(img.Width * ratio), (int)(img.Height * ratio)), 0, 0, InterpolationFlags.Cubic))
+            {
+                Cv2.ImWrite(dstPath, dst);
             }
         }
     }
